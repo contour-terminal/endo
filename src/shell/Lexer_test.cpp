9cf67899ea1216482b9d3d2b5483896e10bc00ed
Christian Parpart <christian@parpart.family>
2023-09-05 21:54:40 +0200

    Add some basic lexer tests
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- src/shell/Lexer_test.cpp
+++ src/shell/Lexer_test.cpp
@@ -3,6 +3,24 @@
 
 #include <shell/Lexer.h>
 
-TEST_CASE("Lexer")
+TEST_CASE("Lexer.basic")
 {
+    auto lexer = crush::Lexer(std::make_unique<crush::StringSource>("echo hello world $PATH"));
+    CHECK(lexer.currentToken() == crush::Token::Identifier);
+    CHECK(lexer.currentLiteral() == "echo");
+
+    lexer.nextToken();
+    CHECK(lexer.currentToken() == crush::Token::Identifier);
+    CHECK(lexer.currentLiteral() == "hello");
+
+    lexer.nextToken();
+    CHECK(lexer.currentToken() == crush::Token::Identifier);
+    CHECK(lexer.currentLiteral() == "world");
+
+    lexer.nextToken();
+    CHECK(lexer.currentToken() == crush::Token::DollarName);
+    CHECK(lexer.currentLiteral() == "PATH");
+
+    lexer.nextToken();
+    CHECK(lexer.currentToken() == crush::Token::EndOfInput);
 }
